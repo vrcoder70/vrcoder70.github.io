e5cc59bb886cee8885db7c6163e68cef44d96cd0
Vraj Rana <vrajrana70@gmail.com>
2024-03-02 04:03:38 -0700

    Refined few details

--- src/Summary.js
+++ src/Summary.js
@@ -13,7 +13,7 @@ function Summary() {
           <div className="col-md-8">
             <h2>Summary</h2>
             <p>
-              Hello, I'm Vraj, a dedicated Master of Science student majoring in Computer Science at Arizona State University, slated to graduate in May 2024. With two years of professional experience as both a Data Analyst at AIDE Lab and a Systems Engineer at Tata Consultancy Services, I've cultivated a strong foundation in practical skills and industry insights. <br /> <br />
+              Hello, I'm Vraj, a dedicated Master of Science student majoring in Computer Science at Arizona State University, slated to graduate in May 2024. With two years of professional experience as both a Data Analyst at AIDE Lab and a Systems Engineer at Tata Consultancy Services, I've cultivated a strong foundation in practical skills and industry insights. As I approach graduation, I am actively seeking full-time opportunities to apply my expertise and contribute to innovative projects in the field. <br /> <br />
               As an international student, I bring a unique blend of perspectives, multilingual proficiency, cultural awareness, and a global network. My experiences have instilled in me a deep cultural sensitivity, a global mindset, and a robust capacity for adaptability and resilience in diverse environments. I am committed to leveraging my knowledge and skills to excel in the field of computer science, contributing meaningfully to innovative projects and collaborative teams.
             </p>
             <h2>Goals</h2>
