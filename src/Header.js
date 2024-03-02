235358f042ecc507b3b731fcf4927ddfe4e7e1fa
Vraj Rana <vrajrana70@gmail.com>
2024-03-02 14:04:32 -0700

    Name Change

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container">
-        <a className="navbar-brand" href="#">Vraj Rana Tech Portfolio</a>
+        <a className="navbar-brand" href="#">Vraj Rana</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
