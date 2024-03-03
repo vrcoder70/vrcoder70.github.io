2a4dcc0df02e5db8a640c81f020700289b537cd1
Vraj Rana <vrajrana70@gmail.com>
2024-03-03 09:40:33 -0700

    skills change

--- src/Skills.js
+++ src/Skills.js
@@ -4,16 +4,16 @@ import './Skills.css';
 const Skills = () => {
     // Define your skills here
     const skills = [
-        "Python", "Java", "JavaScript", "C++", "SQL", "NoSQL", "Pytorch", "Computer Vision", "CNN", "Data Visualization", "Data Analysis",
-        "Docker", "Kubernetes", "AWS", "Terraform", "Jenkins", "Spring Boot", "JPA", "JPQL", "Rest APIs", "Agile", 
-        "Node.Js", "React.JS", "D3.JS", "Git", "Junit", "Mockito","MongoDB", "PostgreSQL"
+        "Python", "Java", "JavaScript", "C++", "SQL", "NoSQL", "MongoDB", "PostgreSQL", "Pytorch", "Computer Vision", "Convolutional Neural Network", "Data Visualization", "Data Analysis",
+        "Cloud Computing", "Kubernetes", "AWS", "Docker",  "Terraform", "Jenkins", "Spring Boot", "JPA", "JPQL", "Rest APIs", "Agile", "Scrum",
+        "Node.Js", "React.JS", "D3.JS", "Git", "Junit", "Mockito", "JIRA"
     ];
 
     return (
         <section id="skills" className="py-2">
             <div className="container">
                 <h2>Skills</h2>
-                <div className="skills">
+                <div className="skills d-flex" style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
                 {skills.map((skill, index) => (
                     <div key={index} className="skill">{skill}</div>
                 ))}
